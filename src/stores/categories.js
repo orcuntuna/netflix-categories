1792bb8189448f14c253d135cf1939eb0f8918a4
orcuntuna <turboindirblog@gmail.com>
2020-05-05 00:47:39 +0300

    update categories data

--- src/stores/categories.js
+++ src/stores/categories.js
@@ -5,49 +5,49 @@ const categories = [
     other: [
       {
         name: "Asian Action Movies",
-        code: 77232
+        code: 77232,
       },
       {
         name: "Classic Action & Adventure",
-        code: 46576
+        code: 46576,
       },
       {
         name: "Action Comedies",
-        code: 43040
+        code: 43040,
       },
       {
         name: "Action Thrillers",
-        code: 43048
+        code: 43048,
       },
       {
         name: "Adventures",
-        code: 7442
+        code: 7442,
       },
       {
         name: "Comic Book and Superhero Movies",
-        code: 10118
+        code: 10118,
       },
       {
         name: "Westerns",
-        code: 7700
+        code: 7700,
       },
       {
         name: "Spy Action & Adventure",
-        code: 10702
+        code: 10702,
       },
       {
         name: "Foregin Action & Adventure",
-        code: 11828
+        code: 11828,
       },
       {
         name: "Martial Arts Movies",
-        code: 8985
+        code: 8985,
       },
       {
         name: "Military Action & Adventure",
-        code: 2125
-      }
-    ]
+        code: 2125,
+      },
+    ],
   },
   {
     name: "Anime",
@@ -55,39 +55,193 @@ const categories = [
     other: [
       {
         name: "Adult Animation",
-        code: 11881
+        code: 11881,
       },
       {
         name: "Anime Action",
-        code: 2653
+        code: 2653,
       },
       {
         name: "Anime Comedies",
-        code: 9302
+        code: 9302,
       },
       {
         name: "Anime Dramas",
-        code: 452
+        code: 452,
       },
       {
         name: "Anime Features",
-        code: 3063
+        code: 3063,
       },
       {
         name: "Anime Sci-Fi",
-        code: 2729
+        code: 2729,
       },
       {
         name: "Anime Horror",
-        code: 10695
+        code: 10695,
       },
       {
         name: "Anime Fantasty",
-        code: 11146
+        code: 11146,
       },
       {
         name: "Anime Series",
-        code: 6721
+        code: 6721,
+      },
+    ],
+  },
+  {
+    name: "Children & Family Movies",
+    code: 783,
+    other: [
+      {
+        name: "Movies for ages 0 to 2",
+        code: 6796,
+      },
+      {
+        name: "Movies for ages 2 to 4",
+        code: 6218,
+      },
+      {
+        name: "Movies for ages 5 to 7",
+        code: 5455,
+      },
+      {
+        name: "Movies for ages 8 to 10",
+        code: 561,
+      },
+      {
+        name: "Movies for ages 11 to 12",
+        code: 6962,
+      },
+      {
+        name: "Education for Kids",
+        code: 10659,
+      },
+      {
+        name: "Disney",
+        code: 67673,
+      },
+      {
+        name: "Movies based on children's books",
+        code: 10056,
+      },
+      {
+        name: "Family Features",
+        code: 51056,
+      },
+      {
+        name: "TV Cartoons",
+        code: 11177,
+      },
+      {
+        name: "Kids TV",
+        code: 27346,
+      },
+      {
+        name: "Kids Music",
+        code: 52843,
+      },
+      {
+        name: "Animal Tales",
+        code: 5507,
+      },
+    ],
+  },
+  {
+    name: "Classic Movies",
+    code: 31574,
+    other: [
+      {
+        name: "Classic Comedies",
+        code: 31694,
+      },
+      {
+        name: "Classic Dramas",
+        code: 29809,
+      },
+      {
+        name: "Classic Sci-Fi & Fantasty",
+        code: 47147,
+      },
+      {
+        name: "Film Noir",
+        code: 7687,
+      },
+      {
+        name: "Classic War Movies",
+        code: 48744,
+      },
+      {
+        name: "Epics",
+        code: 52858,
+      },
+      {
+        name: "Classic Foreign Movies",
+        code: 32473,
+      },
+      {
+        name: "Slient Movies",
+        code: 53310,
+      },
+      {
+        name: "Classic Westerns",
+        code: 47465,
+      },
+    ],
+  },
+  {
+    name: "Comedies",
+    code: 6548,
+    other: [
+      {
+        name: "Dark Comedies",
+        code: 869
+      },
+      {
+        name: "Foreign Comedies",
+        code: 4426
+      },
+      {
+        name: "Late Night Comedies",
+        code: 1402
+      },
+      {
+        name: "Mockumentaries",
+        code: 26
+      },
+      {
+        name: "Political Comedies",
+        code: 2700
+      },
+      {
+        name: "Screwball Comedies",
+        code: 9702
+      },
+      {
+        name: "Sports Comedies",
+        code: 5286
+      },
+      {
+        name: "Stand-up Comedy",
+        code: 11559
+      },
+      {
+        name: "Teen Comedies",
+        code: 3519
+      },
+      {
+        name: "Satires",
+        code: 4922
+      },
+      {
+        name: "Romantic Comedies",
+        code: 5475
+      },
+      {
+        name: "Slapstick Comedies",
+        code: 10256
       },
     ]
   }
