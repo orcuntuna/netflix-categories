699512aa5fe2ec9e3801108d75cf371f363996a3
orcuntuna <turboindirblog@gmail.com>
2020-05-04 23:54:08 +0300

    filter component

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,12 @@
 <script>
   import "bootstrap/dist/css/bootstrap.min.css";
   import Header from "./components/Header.svelte";
+  import Filter from "./components/Filter.svelte";
 </script>
 
 <main>
   <Header />
+  <div class="container">
+    <Filter />
+  </div>
 </main>
---
+++ src/components/Filter.svelte
@@ -0,0 +1,29 @@
+<style>
+  .filter {
+    margin-top: 30px;
+    position: relative;
+  }
+  input{
+    color: #ddd;
+    background: #262626;
+    width: 100%;
+    border: none;
+    border-radius: 5px;
+    height: 50px;
+    outline: 0;
+    padding: 0 20px;
+  }
+  input::placeholder{
+    color: #a1a1a1;
+  }
+  .icon{
+    position: absolute;
+    right: 20px;
+    top: 12px;
+  }
+</style>
+
+<div class="filter">
+  <img class="icon" src="img/search.svg" alt="search">
+  <input type="text" placeholder="Search and filter categories.." />
+</div>
