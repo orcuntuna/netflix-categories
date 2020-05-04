e25430c50b3320e8c7efbf4168d98d98d9f4a8f7
orcuntuna <turboindirblog@gmail.com>
2020-05-04 23:46:43 +0300

    css configuration and Header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,32 @@
+<style>
+  .header {
+    background: #222;
+  }
+  .header-in {
+    display: flex;
+    height: 60px;
+    justify-content: space-between;
+    align-items: center;
+  }
+  h1 {
+    font-size: 1.4em;
+    padding: 0;
+    margin: 0;
+  }
+  a {
+    color: #eee;
+    font-size: 0.9em;
+  }
+</style>
+
+<div class="header">
+  <div class="container header-in">
+    <h1>netflix categories</h1>
+    <a
+      href="https://github.com/orcuntuna/netflix-categories"
+      title="GitHub Repository"
+      target="_blank">
+      View on GitHub
+    </a>
+  </div>
+</div>
